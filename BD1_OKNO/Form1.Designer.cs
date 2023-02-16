de9ad64dce096d6b60bbbaeda2a67ecd99cdffc0
Александр Кожаринов <nioder125@gmail.com>
2023-02-17 01:10:07 +0300

    Refactoring
    bla bla bla

--- BD1_OKNO/Form1.Designer.cs
+++ BD1_OKNO/Form1.Designer.cs
@@ -53,7 +53,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Ввод";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.OnEnterbuttonClick);
             // 
             // textBox1
             // 
@@ -153,7 +153,7 @@
             this.button2.TabIndex = 11;
             this.button2.Text = "Заполнение и расчет";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.OnCalculateButtonClick);
             // 
             // label6
             // 
--- BD1_OKNO/Form1.cs
+++ BD1_OKNO/Form1.cs
@@ -2,10 +2,9 @@ namespace BD1_OKNO
 {
     public partial class Form1 : Form
     {
-        int N = 0;
-        int D;
-        int E;
-        bool isOpen;
+        private int arrayElementsCount = 0;
+        private int leftBorder;
+        private int rightBorder;
         
         public Form1()
         {
@@ -30,82 +29,106 @@ namespace BD1_OKNO
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void HideTables()
+        {
+            dataGridView1.Visible = false;
+            dataGridView2.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            button2.Visible = false;
+            Height = 185;
+        }
+
+        private void ShowTables()
+        {
+            Height = 618;
+            dataGridView1.Visible = true;
+            dataGridView2.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            button2.Visible = true;
+        }
+
+        private bool ValidateTableParameters()
         {
             try
             {
-                N = Convert.ToInt32(textBox1.Text);
-                D = Convert.ToInt32(textBox2.Text);
-                E = Convert.ToInt32(textBox3.Text);
+                arrayElementsCount = Convert.ToInt32(textBox1.Text);
+                leftBorder = Convert.ToInt32(textBox2.Text);
+                rightBorder = Convert.ToInt32(textBox3.Text);
             }
-            catch (Exception ex)
+            catch (FormatException)
             {
-                isOpen = false;
-                MessageBox.Show("Проверьте правильность введенных данных", "Ошибка");
+                HideTables();
+                MessageBox.Show("Проверьте правильность введенных данных.", "Ошибка");
+                return false;
             }
-            
-            
-            if (N == 0)
+            catch (OverflowException)
             {
-                dataGridView1.RowCount = 1;
+                HideTables();
+                MessageBox.Show("Значение слишком большое.", "Ошибка");
+                return false;
             }
-            if (N > 0)
-            {
-                isOpen = true;
-            }
-            dataGridView1.RowCount = N;
-            
-            for (int i = 0; i < N; i++)
+            return true;
+        }
+
+        private void OnEnterbuttonClick(object sender, EventArgs e)
+        {
+            if (!ValidateTableParameters()) return;
+
+            dataGridView1.RowCount = arrayElementsCount;
+
+            for (int i = 0; i < arrayElementsCount; i++)
             {
                 dataGridView1.Rows[i].Cells[0].Value = i; 
             }
-            if (isOpen == true)
+
+            if (arrayElementsCount > 0)
             {
-                this.Height = 618;
-                dataGridView1.Visible = true;
-                dataGridView2.Visible = true;
-                label4.Visible = true;
-                label5.Visible = true;
-                label6.Visible = true;
-                button2.Visible = true;
+                ShowTables();
             }
             else
             {
-                dataGridView1.Visible = false;
-                dataGridView2.Visible = false;
-                label4.Visible = false;
-                label5.Visible = false;
-                label6.Visible = false;
-                button2.Visible = false;
-                this.Height = 185;
+                HideTables();
             }
-            
-
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private bool OutOfBorders(int n)
+        {
+            return n < leftBorder || n > rightBorder;
+        }
+
+        private void OnCalculateButtonClick(object sender, EventArgs e)
         {
             int[] Xn = new int[dataGridView1.Rows.Count];
-            try
+
+            if (!ValidateTableParameters()) return;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
             {
-                N = Convert.ToInt32(textBox1.Text);
-                D = Convert.ToInt32(textBox2.Text);
-                E = Convert.ToInt32(textBox3.Text);
-                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                if (dataGridView1.Rows[i].Cells[1].Value == null)
                 {
-                    if (dataGridView1.Rows[i].Cells[i].Value != null)
-                        Xn[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+                    MessageBox.Show($"Значение не поодерживается. (Строка {i})", "Ошибка");
+                    return;
+                }
+                try
+                {
+                    Xn[i] = Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show($"Значение не поодерживается. (Строка {i})", "Ошибка");
+                    return;
                 }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Проверьте правильность введенных данных", "Ошибка");
-            }
+
             int nOperations = 0;
             int proizved = 1;
             for (int i = 0; i < Xn.Length; i++)
             {
-                if (Xn[i] < D || Xn[i] > E)
+                if (OutOfBorders(Xn[i]))
                 {
                     proizved *= Xn[i];
                     nOperations++;
@@ -116,8 +139,7 @@ namespace BD1_OKNO
 
             label6.Text = "Произведение элементов, значения которых не принадлежат отрезку D,E: " + proizved;
 
-            var selectedBy3 = Xn.Where(item => item % 3 == 0);
-            int[] Yn = selectedBy3.ToArray();
+            int[] Yn = Xn.Where(item => item % 3 == 0).ToArray();
             dataGridView2.RowCount = Yn.Length;
 
             for (int i = 0; i < Yn.Length; i++)
@@ -125,7 +147,6 @@ namespace BD1_OKNO
                 dataGridView2.Rows[i].Cells[0].Value = i;
                 dataGridView2.Rows[i].Cells[1].Value = Yn[i];
             }
-            
         }
     }
 }
